24dabf9932a919cf797e9116becfa0208fc6df56
Salikh Magomedov <salikh.magomedov@icloud.com>
2021-01-22 12:16:20 +0300

    Refactoring WalshTransform.cs

--- Tests/WalshTransformTests.cs
+++ Tests/WalshTransformTests.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using FluentAssertions;
 using NUnit.Framework;
+using WalshSystem;
 using static WalshSystem.WalshTransform;
 
 namespace Tests
@@ -10,23 +11,21 @@ namespace Tests
     public class WalshTransformTests
     {
         private static double F(double x) =>
-            // Math.Sin(6 * Math.PI * t) + t;
             x * x;
 
         private static double Df(double x) =>
-            // 6 * Math.PI * Math.Cos(6 * Math.PI * x) + 1;
             2 * x;
 
         [Test]
         public void ForwardTransform_InputArray_ReturnCorrectResult()
         {
-            ForwardTransform(new[] {1.0, 0, 1, 0}).Should().Equal(.5, 0, .5, 0);
+            ForwardTransform(new[] {1.0, 0, 1, 0}).Should().Equal(2.0, 0, 2.0, 0);
         }
 
         [Test]
         public void InverseTransform_InputCoefficientArray_ReturnCorrectResult()
         {
-            InverseTransform(new[] {.5, 0, .5, 0}).Should().Equal(1, 0, 1, 0);
+            InverseTransform(new[] {2.0, 0, 2.0, 0}).Should().Equal(1, 0, 1, 0);
         }
 
         [Test]
@@ -48,42 +47,36 @@ namespace Tests
         [Test]
         public void FastWalshHadamardTransformTest()
         {
-            FastWalshHadamardTransform(new[] {1, 0, 1, 0, 0, 1, 1, 0.0})
-                .Should().Equal(.5, .25, 0, -.25, 0, .25, 0, .25);
+            FastWalshTransform(new[] {1, 0, 1, 0, 0, 1, 1, 0.0}, WalshMatrixOrdering.Natural)
+                .Should().Equal(4, 2, 0, -2, 0, 2, 0, 2);
         }
 
         [Test]
         public void InverseFastWalshHadamardTransformTest()
         {
             var x = new[] {1, 0, 1, 0, 0, 1, 1, 0.0};
-            var y = FastWalshHadamardTransform(x).ToArray();
+            var y = FastWalshTransform(x, WalshMatrixOrdering.Natural).ToArray();
             
-            InverseFastWalshHadamardTransform(y).Should().Equal(x);
+            InverseFastWalshTransform(y, WalshMatrixOrdering.Natural).Should().Equal(x);
         }
 
-        [TestCase(1)]
-        [TestCase(2)]
-        [TestCase(3)]
         [TestCase(4)]
         [TestCase(5)]
         [TestCase(6)]
-        [TestCase(7)]
         public void PartialSumOneKTest(int k)
         {
             var n = 1 << k;
             var x = Enumerable.Range(0, n).Select(i => i / (n - 1.0)).ToArray();
-            
-            var s = PartialSumOneK(F, n);
+
+            var c = FastWalshSobolevTransform(F, k);
+            var s = PartialSumOneK(F(0), c);
             
             var actual = x.Select(d => s(d));
             var expected = x.Select(F);
 
-            actual.Should().Equal(expected, (a, e) => Math.Abs(a - e) < 1e-2);
+            actual.Should().Equal(expected, (a, e) => Math.Abs(a - e) < 1e-3);
         }
 
-        [TestCase(1)]
-        [TestCase(2)]
-        [TestCase(3)]
         [TestCase(4)]
         [TestCase(5)]
         [TestCase(6)]
@@ -91,13 +84,13 @@ namespace Tests
         {
             var x = Enumerable.Range(0, 1 << k).Select(i => i / ((1 << k) - 1.0)).ToArray();
 
-            var c = FastWalshSobolevTransform(Df, k).ToArray();
+            var c = FastWalshSobolevTransform(F, k).ToArray();
             var y = InverseFastWalshSobolevTransform(c, F(0));
 
             var actual = x.Select(d => y(d));
             var expected = x.Select(F);
             
-            actual.Should().Equal(expected, (a, e) => Math.Abs(a - e) < 1e-2);
+            actual.Should().Equal(expected, (a, e) => Math.Abs(a - e) < 1e-3);
         }
 
         [Test]
@@ -112,11 +105,9 @@ namespace Tests
             InverseFastWalshTransform(c).Should().Equal(f, (a, e) => Math.Abs(a - e) <= 1e-10);
         }
 
-        [TestCase(1)]
-        [TestCase(2)]
-        [TestCase(3)]
         [TestCase(4)]
         [TestCase(5)]
+        [TestCase(6)]
         public void FastWalshTest(int k)
         {
             var n = 1 << k;
@@ -124,19 +115,24 @@ namespace Tests
             var c1 = FastWalshSobolevTransform(F, k);
             var c2 = Enumerable.Range(1, n)
                 .Select(i => GetCoefficientOneK(F, i));
-            var c3 = Enumerable.Range(1, n)
-                .Select(i => GetCoefficientOneK(F, Df, i));
 
+            c1.Should().Equal(c2, (a, b) => Math.Abs(a - b) < 1e-10);
+        }
 
-            bool Comparison(double a, double b)
-            {
-                return Math.Abs(a - b) < 1e-10;
-            }
+        [Test]
+        public void FastWalshSobolevTransform_MustBeEqual_ForwardTransformDF()
+        {
+            const int k = 4;
+            const int n = 1 << k;
+            
+            var g = Enumerable.Range(0, n)
+                .Select(i => F((i + 1.0) / n) - F((double) i / n))
+                .ToArray();
 
-            c1.Should()
-                .Equal(c2, Comparison)
-                .And
-                .Equal(c3, Comparison);
+            var fast = FastWalshSobolevTransform(F, k);
+            var slow = ForwardTransform(g);
+
+            fast.Should().Equal(slow);
         }
     }
 }
--- WalshSystem/WalshTransform.cs
+++ WalshSystem/WalshTransform.cs
@@ -13,116 +13,12 @@ namespace WalshSystem
                 .Sum();
         }
 
-        private static IEnumerable<double> BaseTransform(IReadOnlyList<double> y, WalshMatrixOrdering ordering)
+        public static Func<double, double> PartialSumOneK(double f0, IEnumerable<double> coefficients)
         {
-            var length = y.Count;
+            IEnumerable<(double c, Func<double, double> w)> data =
+                coefficients.Select((c, i) => (c, Walsh.GetOneK(i + 1)));
 
-            var k = (int) Math.Log(length, 2);
-            var w = Walsh.GetMatrix(k, ordering);
-            for (var i = 0; i < length; i++)
-            {
-                var s = 0.0;
-                for (var j = 0; j < length; j++)
-                {
-                    s += y[j] * w[i, j];
-                }
-
-                yield return s;
-            }
-        }
-
-        public static IEnumerable<double> ForwardTransform(IReadOnlyList<double> y, WalshMatrixOrdering ordering = WalshMatrixOrdering.Dyadic)
-        {
-            return BaseTransform(y, ordering).Select(d => d / y.Count).ToArray();
-        }
-
-        public static IEnumerable<double> InverseTransform(double[] coefficients,
-            WalshMatrixOrdering ordering = WalshMatrixOrdering.Dyadic)
-        {
-            return BaseTransform(coefficients, ordering);
-        }
-
-        private static IEnumerable<double> BaseFastWalshHadamardTransform(double[] array)
-        {
-            var result = new double[array.Length];
-            array.CopyTo(result, 0);
-
-            var h = 1;
-            while (h < result.Length)
-            {
-                for (var i = 0; i < result.Length; i += h * 2)
-                {
-                    for (var j = i; j < i + h; j++)
-                    {
-                        var x = result[j];
-                        var y = result[j + h];
-
-                        result[j] = x + y;
-                        result[j + h] = x - y;
-                    }
-                }
-
-                h *= 2;
-            }
-
-            return result;
-        }
-
-        public static IEnumerable<double> FastWalshHadamardTransform(double[] array)
-        {
-            return BaseFastWalshHadamardTransform(array).Select(d => d / array.Length);
-        }
-
-        public static IEnumerable<double> InverseFastWalshHadamardTransform(double[] coefficients)
-        {
-            return BaseFastWalshHadamardTransform(coefficients);
-        }
-
-        private static int BinaryInverse(int number, int n)
-        {
-            var result = 0;
-
-            for (int i = 1, pow2K = 1 << (n - 1); i <= n; i++, pow2K >>= 1)
-            {
-                if (number == 0) break;
-                
-                result += number / pow2K * (1 << (i - 1));
-                number %= pow2K;
-            }
-            
-            return result;
-        }
-
-        private static double[] BinaryReversedArray(IReadOnlyList<double> array)
-        {
-            var result = new double[array.Count];
-            var n = (int) Math.Log(result.Length, 2);
-
-            for (var i = 0; i < result.Length; i++)
-            {
-                result[BinaryInverse(i, n)] = array[i];
-            }
-
-            return result;
-        }
-
-        public static IEnumerable<double> FastWalshTransform(double[] array)
-        {
-            var result = BinaryReversedArray(array);
-
-            return FastWalshHadamardTransform(result);
-        }
-
-        public static IEnumerable<double> InverseFastWalshTransform(double[] coefficients)
-        {
-            var result = BinaryReversedArray(coefficients);
-            
-            return BaseFastWalshHadamardTransform(result);
-        }
-
-        public static double GetCoefficientOneK(Func<double, double> f, Func<double, double> df, int k, int n = 1 << 6)
-        {
-            return Integral.Rectangular(t => df(t) * Walsh.Get(k - 1)(t), n: n);
+            return x => f0 + data.Select(d => d.c * d.w(x)).Sum();
         }
 
         public static double GetCoefficientOneK(Func<double, double> f, int k)
@@ -144,61 +40,146 @@ namespace WalshSystem
             return result;
         }
 
-        public static Func<double, double> PartialSumOneK(Func<double, double> f, int n)
+        public static IEnumerable<double> ForwardTransform(IReadOnlyList<double> y,
+            WalshMatrixOrdering ordering = WalshMatrixOrdering.Dyadic)
         {
-            IEnumerable<(double c, Func<double, double> w)> data = Enumerable.Range(1, n)
-                .Select(i => (GetCoefficientOneK(f, i), Walsh.GetOneK(i)));
+            var length = y.Count;
+
+            var k = (int) Math.Log(length, 2);
+            var w = Walsh.GetMatrix(k, ordering);
+            for (var i = 0; i < length; i++)
+            {
+                var s = 0.0;
+                for (var j = 0; j < length; j++)
+                {
+                    s += y[j] * w[i, j];
+                }
+
+                yield return s;
+            }
+        }
+
+        public static IEnumerable<double> InverseTransform(IReadOnlyList<double> coefficients,
+            WalshMatrixOrdering ordering = WalshMatrixOrdering.Dyadic)
+        {
+            return ForwardTransform(coefficients, ordering).Select(d => d / coefficients.Count);
+        }
+
+        public static IEnumerable<double> FastWalshTransform(IReadOnlyList<double> array,
+            WalshMatrixOrdering ordering = WalshMatrixOrdering.Dyadic)
+        {
+            static double[] BinaryReversedArray(IReadOnlyList<double> array)
+            {
+                static int BinaryInverse(int number, int n)
+                {
+                    var result = 0;
+
+                    for (int i = 1, pow2K = 1 << (n - 1); i <= n; i++, pow2K >>= 1)
+                    {
+                        if (number == 0) break;
+                
+                        result += number / pow2K * (1 << (i - 1));
+                        number %= pow2K;
+                    }
             
-            return x => f(0) + data.Select(d => d.c * d.w(x)).Sum();
+                    return result;
+                }
+
+                var result = new double[array.Count];
+                var n = (int) Math.Log(result.Length, 2);
+
+                for (var i = 0; i < result.Length; i++)
+                {
+                    result[BinaryInverse(i, n)] = array[i];
+                }
+
+                return result;
+            }
+            
+            static IEnumerable<double> FastWalshHadamardTransform(IReadOnlyCollection<double> vector)
+            {
+                var result = vector.ToArray();
+
+                var h = 1;
+                while (h < vector.Count)
+                {
+                    for (var i = 0; i < vector.Count; i += h * 2)
+                    {
+                        for (var j = i; j < i + h; j++)
+                        {
+                            var x = result[j];
+                            var y = result[j + h];
+
+                            result[j] = x + y;
+                            result[j + h] = x - y;
+                        }
+                    }
+
+                    h *= 2;
+                }
+
+                return result;
+            }
+
+            return FastWalshHadamardTransform(ordering switch
+            {
+                WalshMatrixOrdering.Natural => array,
+                _ => BinaryReversedArray(array)
+            });
         }
 
-        public static Func<double, double> PartialSumOneK(double f0, IEnumerable<double> coefficients)
+        public static IEnumerable<double> InverseFastWalshTransform(IReadOnlyList<double> coefficients,
+            WalshMatrixOrdering ordering = WalshMatrixOrdering.Dyadic)
         {
-            IEnumerable<(double c, Func<double, double> w)> data =
-                coefficients.Select((c, i) => (c, Walsh.GetOneK(i + 1)));
-
-            return x => f0 + data.Select(d => d.c * d.w(x)).Sum();
+            return FastWalshTransform(coefficients, ordering).Select(d => d / coefficients.Count);
         }
 
-        public static IEnumerable<double> FastWalshSobolevTransform(Func<double, double> f, int k)
+        public static IEnumerable<double> FastWalshSobolevTransform(Func<double, double> f, int k,
+            WalshMatrixOrdering ordering = WalshMatrixOrdering.Dyadic)
         {
             var n = 1 << k;
 
             var arr = Enumerable.Range(0, n)
-                .Select(i => (f((i + 1.0) / n) - f((double) i / n)) * n)
+                .Select(i => f((i + 1.0) / n) - f((double) i / n))
                 .ToArray();
 
-            return FastWalshTransform(arr);
+            return FastWalshTransform(arr, ordering);
         }
 
-        public static Func<double, double> InverseFastWalshSobolevTransform(double[] coefficients, double f0)
+        public static Func<double, double> InverseFastWalshSobolevTransform(IReadOnlyList<double> coefficients,
+            double f0, WalshMatrixOrdering ordering = WalshMatrixOrdering.Dyadic)
         {
-            var df = InverseFastWalshTransform(coefficients).ToArray();
-            var s = new double[df.Length + 1];
+            var g = InverseFastWalshTransform(coefficients, ordering).ToArray();
+            var s = new double[g.Length + 1];
 
             s[0] = f0;
 
-            for (var i = 0; i < df.Length; i++)
+            for (var i = 0; i < g.Length; i++)
             {
-                s[i + 1] = s[i] + df[i] / df.Length;
+                s[i + 1] = s[i] + g[i];
             }
 
             return x =>
             {
-                if (x <= 0) return s[0];
-                if (x >= 1) return s[df.Length];
+                switch (x)
+                {
+                    case <= 0:
+                        return s[0];
+                    case >= 1:
+                        return s[g.Length];
+                }
 
-                var index = (int) (x * df.Length);
+                var index = (int) (x * g.Length);
 
-                if (Math.Abs(index - x * df.Length) < double.Epsilon)
+                if (Math.Abs(index - x * g.Length) < double.Epsilon)
                 {
                     return s[index];
                 }
                 
-                var lp = (double) index / df.Length;
+                var lp = (double) index / g.Length;
                 var ls = s[index];
                 var rs = s[index + 1];
-                var p = (x - lp) * df.Length;
+                var p = (x - lp) * g.Length;
                 
                 return ls + (rs - ls) * p;
             };
